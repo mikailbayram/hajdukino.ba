fc69822933cc50df50e7afd0a65c5f68b819114e
Mikail <mikailbosna98@gmail.com>
2018-05-25 16:39:44 +0200

    user validation

--- rest/PersistanceManager.class.php
+++ rest/PersistanceManager.class.php
@@ -8,6 +8,27 @@ class PersistanceManager{
     $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   }
   public function add_user($user){
+
+    $usernameCheck = $this->pdo->prepare("SELECT username FROM users WHERE username  = :name");
+    $usernameCheck->bindParam(':name', $user['username']);
+    $usernameCheck->execute();
+
+    $emailCheck = $this->pdo->prepare("SELECT email FROM users WHERE email  = :email");
+    $emailCheck->bindParam(':email', $user['email']);
+    $emailCheck->execute();
+
+    if($usernameCheck->rowCount() > 0)
+      $x["error"]["username"] = "username already exists";
+
+    if($emailCheck->rowCount() > 0)
+      $x["error"]["email"] = "email already exists";
+    
+    if(strlen($user['pass'])<6)
+      $x["error"]["password"] = "password should be atleast 6 characters long";
+
+    if($x["error"])
+      return $x;
+    
     $query = "INSERT INTO users
             (username,
              email,
