e17e290aa2781971e908135fb0e2c9410472272c
Mikail <mikailbosna98@gmail.com>
2018-05-28 14:04:48 +0200

    add movie

--- rest/PersistanceManager.class.php
+++ rest/PersistanceManager.class.php
@@ -27,22 +27,37 @@ class PersistanceManager{
       return $x["error"]["password"] = "password should be atleast 6 characters long";
 
     
+    $cinemaQuery = "INSERT INTO Cinemas
+    (name)
+    VALUES (:cinema_name)";  
+    $statement = $this->pdo->prepare($cinemaQuery);
+    $statement->bindParam(':cinema_name', $user['cinema_name']);
+    $statement->execute();
+    $user['cinema_id'] = $this->pdo->lastInsertId();
+
     $query = "INSERT INTO users
             (username,
              email,
-             password)
+             password,
+             cinema_id)
             VALUES (:username,
                     :email,
-                    :pass)";
+                    :pass,
+                    :cinema_id)";           
     $statement = $this->pdo->prepare($query);
-    $statement->execute($user);
+    $statement->bindParam(':username', $user['username']);
+    $statement->bindParam(':email', $user['email']);
+    $statement->bindParam(':pass', $user['pass']);
+    $statement->bindParam(':cinema_id', $user['cinema_id']);
+    $statement->execute();
+
     $user['id'] = $this->pdo->lastInsertId();
     return $user;
   }
 
   //find if username matches password
   public function find_user($user){
-    $usernameCheck = $this->pdo->prepare("SELECT username FROM users WHERE username  = :name AND password = :password");
+    $usernameCheck = $this->pdo->prepare("SELECT * FROM users WHERE username  = :name AND password = :password");
     $usernameCheck->bindParam(':name', $user['username']);
     $usernameCheck->bindParam(':password', $user['pass']);
     $usernameCheck->execute();
@@ -52,10 +67,35 @@ class PersistanceManager{
       $x["error"] = "invalid username or password";
       return $x;
     }
-    $x = $user;
-    $x["valid"] = true;
+  
+    $x=$usernameCheck->fetch(PDO::FETCH_ASSOC);
+    $x["valid"]=true;
     return $x;
 
+  }
+  public function add_movie($movie){
+    $query = "INSERT INTO Movies
+            (name,
+             description,
+             cinema_id)
+            VALUES (:name,
+                    :description,
+                    :cinema_id)";           
+    $statement = $this->pdo->prepare($query);
+    $statement->bindParam(':name', $movie['name']);
+    $statement->bindParam(':description', $movie['description']);
+    $statement->bindParam(':cinema_id', $movie['cinema_id']);
+    $statement->execute();
+
+    $movie['id'] = $this->pdo->lastInsertId();
+    return $movie;  
+  }
+  public function get_all_movies($cinema_id){
+    $statement = $this->pdo->prepare("SELECT * FROM Movies WHERE cinema_id  = :cinema_id");
+    $statement->bindParam(':cinema_id', $cinema_id);
+    $statement->execute();
+    $x=$statement->fetchAll(PDO::FETCH_ASSOC);
+    return $x;
   }
 }
 ?>
--- rest/index.php
+++ rest/index.php
@@ -3,8 +3,61 @@ require_once '../vendor/autoload.php';
 require_once 'PersistanceManager.class.php';
 require_once 'Config.class.php';
 
+use \Firebase\JWT\JWT;
+
 Flight::register('pm', 'PersistanceManager', [Config::DB]);
 
+Flight::route('POST /user/register', function () {
+    $request = Flight::request();
+    $user = [
+        'username' => $request->data->username,
+        'email' => $request->data->email,
+        'pass' => $request->data->pass,
+        'cinema_name' => $request->data->cinema_name,
+    ];
+    $added_user = Flight::pm()->add_user($user);
+    Flight::json($added_user);
+});
 
+Flight::route('POST /user/login', function () {
+    $request = Flight::request();
+    $user = [
+        'username' => $request->data->username,
+        'pass' => $request->data->pass,
+    ];
+
+    $res = Flight::pm()->find_user($user);
+    if ($res["valid"]) {
+        $key = Config::jwtKey;
+        $token = [
+            "exp" => time() + 36000,
+            "data" => $res,
+        ];
+        $jwt = JWT::encode($token, $key, "HS256");
+        $x["token"] = $jwt;
+        $x["cinema_id"] = $res["cinema_id"];
+        Flight::json($x);} else {
+        Flight::json($res);
+    }
+
+});
+
+Flight::route('POST /movies/create', function () {
+    $request = Flight::request();
+    $key = Config::jwtKey;
+    $user = JWT::decode($request->data->token, $key, ["HS256"]);
+    $movie = [
+        'name' => $request->data->name,
+        'description' => $request->data->description,
+        'cinema_id' => $user->data->cinema_id,
+    ];
+
+    $res = Flight::pm()->add_movie($movie);
+    Flight::json($res);
+});
+
+Flight::route('GET /movies/all/@id', function ($id) {
+    $res = Flight::pm()->get_all_movies($id);
+    Flight::json($res);
+});
 Flight::start();
-?>  
